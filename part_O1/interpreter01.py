e351e8453bc45646730fb8ace0421b819c69a89d
Loknath Dhar <loknath-dhar@pm.me>
2020-02-16 14:58:28 +0600

    Interpreter: Addition

--- part_O1/interpreter01.py
+++ part_O1/interpreter01.py
@@ -1,35 +1,45 @@
 # Token types
-# EOF (end-of-file) token is indicate that
+
+# EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
+
 INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+
 
 class Token(object):
     def __init__(self, type, value):
-        """Token type: INTEGER, PLUS or EOF"""
+        # token type: INTEGER, PLUS, or EOF
         self.type = type
-        # token value: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+' or None
+        # token value: 0, 1, 2. 3, 4, 5, 6, 7, 8, 9, '+', or None
         self.value = value
 
     def __str__(self):
-        """String representation of the class instance"""
-        """ Examples:
-                Token(INTEGER, 3)
-                Token(PLUS '+')
+        
         """
-        retrun 'Token({type}, {value})'.format(
-            type = self.type
-            value = repr(self.value)
+        String representation of the class instance.
+        
+        Examples:
+            Token(INTEGER, 3)
+            Token(PLUS '+')
+        """
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
         )
 
     def __repr__(self):
         return self.__str__()
 
+
 class Interpreter(object):
+    
     def __init__(self, text):
         # client string input, e.g. "3+5"
         self.text = text
+        
         # self.pos is an index into self.text
         self.pos = 0
+        
         # current token instance
         self.current_token = None
 
@@ -37,14 +47,17 @@ class Interpreter(object):
         raise Exception('Error parsing input')
 
     def get_next_token(self):
-        """ Lexical analyzer (also known as scanner or tokenizer)
-
-        This method is responsible for breaking a sentence apart
-        into tokens. One token at a time.
+        
+        """
+        Lexical analyzer (also known as scanner or tokenizer)
+        
+        This method is responsible for breaking a sentence
+        apart into tokens. One token at a time.
+        
         """
         text = self.text
 
-        # is self.pos index past the end of the self.text?
+        # is self.pos index past the end of the self.text ?
         # if so, then return EOF token because there is no more
         # input left to convert into tokens
         if self.pos > len(text) - 1:
@@ -74,14 +87,17 @@ class Interpreter(object):
         # compare the current token type with the passed token
         # type and if they match then "eat" the current token
         # and assign the next token to the self.current_token,
-        # otherwise raise an exception
-        if self.current_token == token_type:
+        # otherwise raise an exception.
+        
+        if self.current_token.type == token_type:
             self.current_token = self.get_next_token()
         else:
             self.error()
 
     def expr(self):
-        """expr -> INTEGER PLUS INTEGER """
+        
+        """expr -> INTEGER PLUS INTEGER"""
+        
         # set current token to the first token taken from the input
         self.current_token = self.get_next_token()
 
@@ -102,7 +118,25 @@ class Interpreter(object):
 
         # at this point INTEGER PLUS INTEGER sequence of tokens
         # has been successfully found and the method can just
-        # return the result of adding two integers, thus effectively
-        # interpreting client input
+        # return the result of adding two integers, thus
+        # effectively interpreting client input
+        
         result = left.value + right.value
         return result
+
+
+def main():
+    while True:
+        try:
+            text = input('calc> ')
+        except EOFError:
+            break
+        if not text:
+            continue
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
